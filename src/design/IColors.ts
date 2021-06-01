f61d7cbbd67f3f956c06e710066911edb50445a7
Martin Rossil <martinrossil@gmail.com>
2021-06-01 11:47:00 +0200

    Added remaining colors to interface.

--- src/design/IColors.ts
+++ src/design/IColors.ts
@@ -2,9 +2,9 @@ import IColorRange from './IColorRange';
 
 export default interface IColors {
     readonly black: IColorRange;
-    /* readonly white: IColorRange;
+    readonly white: IColorRange;
     readonly blue: IColorRange;
     readonly green: IColorRange;
     readonly yellow: IColorRange;
-    readonly red: IColorRange; */
+    readonly red: IColorRange;
 }
